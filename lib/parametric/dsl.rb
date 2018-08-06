e815e4d5f9a5fd33f9702bd7561fdc005a693faf
Ismael Celis <ismaelct@gmail.com>
2018-08-06 16:27:03 +0100

    Make form object DSL support multiple schemas internally

--- lib/parametric/dsl.rb
+++ lib/parametric/dsl.rb
@@ -22,26 +22,39 @@ module Parametric
     #
     #   foo.params # => {title: "A title", age: 20}
     #
+    DEFAULT_SCHEMA_NAME = :schema
+
     def self.included(base)
       base.extend(ClassMethods)
-      base.schema = Parametric::Schema.new
+      base.schemas = {DEFAULT_SCHEMA_NAME => Parametric::Schema.new}
     end
 
     module ClassMethods
       def schema=(sc)
-        @schema = sc
+        @schemas[DEFAULT_SCHEMA_NAME] = sc
+      end
+
+      def schemas=(sc)
+        @schemas = sc
       end
 
       def inherited(subclass)
-        subclass.schema = @schema.merge(Parametric::Schema.new)
+        subclass.schemas = @schemas.each_with_object({}) do |(key, sc), hash|
+          hash[key] = sc.merge(Parametric::Schema.new)
+        end
       end
 
       def schema(options = {}, &block)
-        return @schema unless options.any? || block_given?
+        key = DEFAULT_SCHEMA_NAME
+        current_schema = @schemas[key]
+        unless options.any? || block_given?
+          raise ArgumentError, "No schema with name #{key}" unless current_schema
+          return current_schema
+        end
 
         new_schema = Parametric::Schema.new(options, &block)
-        @schema = @schema.merge(new_schema)
-        after_define_schema(@schema)
+        @schemas[key] = current_schema ? current_schema.merge(new_schema) : new_schema
+        after_define_schema(@schemas[key])
       end
 
       def after_define_schema(sc)
