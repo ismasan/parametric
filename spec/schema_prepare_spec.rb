f343fadc309fb02242a578daf46712aec978be99
Ismael Celis <ismaelct@gmail.com>
2019-08-28 14:55:47 +0100

    Test that prepare blocks are copied over when merging schemas

--- spec/schema_prepare_spec.rb
+++ spec/schema_prepare_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Parametric::Schema do
   it 'passes payload through prepare block, if defined' do
     schema = described_class.new do
-      prepare do |payload, context|
+      prepare do |payload, _context|
         payload[:slug] = payload[:name].to_s.downcase.gsub(/\s+/, '-') unless payload[:slug]
         payload
       end
@@ -11,7 +11,7 @@ describe Parametric::Schema do
       field(:name).policy(:string).present
       field(:slug).policy(:string).present
       field(:variants).policy(:array).schema do
-        prepare do |payload, context|
+        prepare do |payload, _context|
           payload[:slug] = "v: #{payload[:name].to_s.downcase}"
           payload
         end
@@ -40,5 +40,30 @@ describe Parametric::Schema do
     results = schema.resolve({ variants: [ {name: 'no errors'}, {name: 'with errors'}]})
     expect(results.valid?).to be false
     expect(results.errors['$.variants[1]']).to eq ['nope!']
+  end
+
+  it 'copies pre-resolvers to merged schemas' do
+    schema1 = described_class.new do
+      prepare do |payload, _context|
+        payload[:slug] = payload[:name].to_s.downcase.gsub(/\s+/, '-') unless payload[:slug]
+        payload
+      end
+      field(:name).present.type(:string)
+      field(:slug).present.type(:string)
+    end
+
+    schema2 = described_class.new do
+      prepare do |payload, _context|
+        payload[:slug] = "slug-#{payload[:slug]}" if payload[:slug]
+        payload
+      end
+
+      field(:age).type(:integer)
+    end
+
+    schema3 = schema1.merge(schema2)
+
+    results = schema3.resolve({ name: 'Ismael Celis', age: 41 })
+    expect(results.output[:slug]).to eq 'slug-ismael-celis'
   end
 end
