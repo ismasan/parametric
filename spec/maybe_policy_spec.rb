ec65c40c802585869464dfdbbae48096f7b63516
Ismael Celis <ismaelct@gmail.com>
2019-09-26 17:14:10 +0100

    test absent key

--- spec/maybe_policy_spec.rb
+++ spec/maybe_policy_spec.rb
@@ -9,5 +9,6 @@ describe 'maybe policy' do
     expect(schema.resolve({ age: 10 }).output[:age]).to eq 10
     expect(schema.resolve({ age: '10' }).output[:age]).to eq 10
     expect(schema.resolve({ age: nil }).output[:age]).to eq nil
+    expect(schema.resolve({ nope: 1 }).output.key?(:age)).to be false
   end
 end
