654adee4963b4bd4ef4ceccd2c626a56f4bf39ef
Ismael Celis <ismaelct@gmail.com>
2020-02-13 09:43:50 +0000

    Make MaybePolicy delegate validation if value not nil

--- spec/maybe_policy_spec.rb
+++ spec/maybe_policy_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'maybe policy' do
-  specify do
+  specify 'dealing with nil values' do
     schema = Parametric::Schema.new do
       field(:age).maybe(:integer)
     end
@@ -10,6 +10,45 @@ describe 'maybe policy' do
     expect(schema.resolve({ age: '10' }).output[:age]).to eq 10
     expect(schema.resolve({ age: nil }).output[:age]).to eq nil
     expect(schema.resolve({ age: false }).output[:age]).to be false
+    expect(schema.resolve({ nope: 1 }).output.key?(:age)).to be false
+  end
+
+  specify 'interacting with types that validate' do
+    Parametric.policy :validating_integer do
+      exp = /^\d+$/
+
+      validate do |value, _key, _context|
+        !!(value.to_s =~ exp)
+      end
+
+      coerce do |value, _key, _context|
+        if value.to_s =~ exp
+          value.to_i
+        else
+          value
+        end
+      end
+
+      message do
+        'error!'
+      end
+    end
+
+    schema = Parametric::Schema.new do
+      field(:age).maybe(:validating_integer)
+    end
+
+    expect(schema.resolve({ age: 10 }).output[:age]).to eq 10
+    expect(schema.resolve({ age: '10' }).output[:age]).to eq 10
+    schema.resolve({ age: 'nope' }).tap do |r|
+      expect(r.output[:age]).to eq 'nope'
+      expect(r.errors.any?).to be true
+    end
+    schema.resolve({ age: nil }).tap do |r|
+      expect(r.output[:age]).to eq nil
+      expect(r.errors.any?).to be false
+    end
+    expect(schema.resolve({ age: nil }).output[:age]).to eq nil
     expect(schema.resolve({ nope: 1 }).output.key?(:age)).to be false
   end
 end
